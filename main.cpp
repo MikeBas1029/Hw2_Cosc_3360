512fe82e7f651e18a74019b58f60187753e76874
MikeBas1029 <98618592+MikeBas1029@users.noreply.github.com>
2024-03-29 01:25:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <vector>
 #include <string>
 #include <sstream>
@@ -15,8 +16,7 @@ using namespace std;
 //Structs:
 
 struct resource{
-    string type;
-    string instance;
+    map<string, vector<string>> instance;   //key will be the type and the value is the
     int amountRes;
 };
 
@@ -55,6 +55,10 @@ void destroy(){
 }
 
 void calculate(int val){
+
+}
+
+void useResource(){
 
 }
 
@@ -124,15 +128,71 @@ int main(int argc, char** argv) {
         }
     }
 
-    ifstream file2(argv[2]);        //to gather the instances and contain them in the resources vector
+    ifstream file2(argv[2]);            //accessing word file
     if(!file2.is_open()){               //If it can't find file
         cout<< "No file can be found.\n";
         return 1;
     }
-    while(getline(file2, inputLine)){
-        while(inputLine.find(',') != string::npos ){
 
+    int count = 0;
+    while(getline(file2, inputLine)){   //to gather the instances and contain them in the resources vector
+        if(count >= resources[0].amountRes) {    //so it does not exceed the amount of resources
+            cout<< "it has exceed the amount of resources\n";
+            break;
+        }else{
+            if(resources.size() <= cout++){     //if it hasn't been created just yet
+                resource r = {.amountRes = resources[0].amountRes};
+                resources.push_back(r);
+            }
+            if (inputLine.find(':') != string::npos) {
+                string instances = inputLine.substr(inputLine.find(':') + 1);
+
+                stringstream ss(instances);
+                string inst;
+                string type;
+
+                getline(ss, type, ':');   //taking the Food:
+                resources[count].instance[type];//adding the type into the key of the instance map
+
+                while (getline(ss, inst, ',')) {
+                    size_t start = inst.find_first_not_of(" ");
+                    size_t end = inst.find_last_not_of(" ");
+
+                    if (start != string::npos && end != string::npos) {
+                        inst = inst.substr(start, end - start + 1);
+                    }
+
+                    resources[count].instance[type].push_back(inst);
+
+                }
+            }
+            count++;
         }
+    }
+    file2.close();
+
+    /*
+    for (const auto& pair : resources[0].instance) {
+        cout << "Key: " << pair.first << ", Values:";
+        for (const auto& value : pair.second) {
+            cout << " " << value;
+        }
+        cout << std::endl;
+    }
+     */
+
+    int processNum;
+    while(getline(file1, inputLine)){      //reading each process and putting them into p struct
+        inputLine.erase(remove_if(inputLine.begin(), inputLine.end(), [](unsigned char c) { return std::isspace(c); }), inputLine.end());   //to remove any possible spaces
+        if(inputLine.find("process") != string::npos){
+            processNum = stoi(inputLine.substr(inputLine.find("process") + 8, 1));
+            count = 0;
+            //cout<< "This process is: "<< processNum<< endl;
+        }
+        if(processesNum){
+            
+        }
+
     }
 
 
